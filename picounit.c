bb8bb0e81a497943ee0ddea619f68c59baee34ac
James McLean <james@picosystems.com>
2019-09-21 13:58:35 -0400

    Bug fix

--- picounit.c
+++ picounit.c
@@ -72,6 +72,11 @@ pico_assert_str_eq (const char* const p_left_str,
 {
     g_num_asserts++;
 
+    if ((NULL == p_left_str) || (NULL == p_right_str))
+    {
+        return 0;
+    }
+
     if (0 == strcmp(p_left_str, p_right_str))
     {
         return 1;
